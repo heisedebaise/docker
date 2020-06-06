22777a125a6d18eb8bb1a6b792a915ba4945760f
heisedebaise <heisedebaise@hotmail.com>
2020-06-06 08:06:48 +0800

    dev

--- certng:latest/renew.sh
+++ certng:latest/renew.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "$(sed 's/^nameserver /#nameserver /g' /etc/resolv.conf)" > /etc/resolv.conf
+echo 'nameserver 8.8.8.8' >> /etc/resolv.conf
+echo 'nameserver 4.4.4.4' >> /etc/resolv.conf
+
 certbot renew --nginx >> /var/log/certbot
 
 nginx -s reload
