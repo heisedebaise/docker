6bd6ebc06928d0fdc2f3779810e28c3203580d0c
heisedebaise <heisedebaise@hotmail.com>
2020-09-18 23:56:11 +0800

    dev

--- easyocr:latest/main.py
+++ easyocr:latest/main.py
@@ -1,32 +1,38 @@
 import logging
 import http.server
 import socketserver
-import datetime
 import time
 import easyocr
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(filename)s:%(lineno)d %(levelname)s %(message)s',
-                    datefmt='%a, %d %b %Y %H:%M:%S', filename='/easyocr/log', filemode='w')
+                    datefmt='%a, %d %b %Y %H:%M:%S', filename='/easyocr/log', filemode='a')
 
 now = time.time()
-logging.info(datetime.datetime.now(), 'loading model [en,ch_sim]')
+logging.info('loading model [en,ch_sim]')
 reader = easyocr.Reader(['en', 'ch_sim'])
-logging.info(datetime.datetime.now(),
-             'load model [en,ch_sim] in', time.time()-now, 'seconds')
+logging.info('load model [en,ch_sim] in %f seconds', time.time()-now)
 
 
 class Handler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
+
+        file = '/easyocr/image'+self.path
+        t = time.time()
+        try:
+            text = reader.readtext(file)
+        except Exception as e:
+            logging.warn(e)
+        logging.debug('read text from %s=>%s duration=%fseconds',
+                      file,  text, time.time()-t)
+
         self.send_response(200)
         self.send_header('Content-type', 'text/html')
         self.end_headers()
-
-        self.wfile.write(reader.readtext(
-            ('/easyocr/image'+self.path).encode('utf-8')))
+        self.wfile.write(str(text).encode('utf-8'))
 
         return
 
 
 with socketserver.TCPServer(("", 8080), Handler) as httpd:
-    logging.info(datetime.datetime.now(), 'listening http on 8080')
+    logging.info('listening http on 8080')
     httpd.serve_forever()
