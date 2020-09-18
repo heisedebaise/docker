34b1067b21f5df4f8a7b17a7a9be212334f76cc6
heisedebaise <heisedebaise@hotmail.com>
2020-09-18 21:16:59 +0800

    dev

--- tomcat:9.0/run.sh
+++ tomcat:9.0/run.sh
@@ -12,7 +12,7 @@ bin/startup.sh &
 today=`date +%Y%m%d`
 while [[ true ]]
 do
-    if [ today != `date +%Y%m%d` ];then
+    if [ $today != `date +%Y%m%d` ];then
         cp /data/logs/catalina.out /data/logs/catalina-$today.out
         today=`date +%Y%m%d`
         echo > /data/logs/catalina.out
