1c847814ecf963102a32d6708738a7872030135b
heisedebaise <heisedebaise@hotmail.com>
2020-09-18 22:08:39 +0800

    dev

--- easyocr:latest/main.py
+++ easyocr:latest/main.py
@@ -5,7 +5,7 @@ import datetime
 import time
 import easyocr
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(filename)s:%(lineno)d %(levelname)s %(message)s',
                     datefmt='%a, %d %b %Y %H:%M:%S', filename='/easyocr/log', filemode='w')
 
 now = time.time()
