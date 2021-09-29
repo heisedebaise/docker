590f1d83cabdb8976f007d0a2b09f924609245a2
heisedebaise <heisedebaise@hotmail.com>
2021-09-29 16:21:48 +0800

    dev

--- certng:latest/renew.sh
+++ certng:latest/renew.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-count=`grep '8.8.8.8' /etc/resolv.conf | wc -l`
-if [ $count -eq 0 ]; then
+if [ `grep '8.8.8.8' /etc/resolv.conf | wc -l` -eq 0 ]; then
     echo 'nameserver 8.8.8.8' >> /etc/resolv.conf
     echo 'nameserver 4.4.4.4' >> /etc/resolv.conf
 fi
