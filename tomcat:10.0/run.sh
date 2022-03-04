beec2706856367b91f3a87a540d2c85f8bdb7550
heisedebaise <heisedebaise@hotmail.com>
2022-03-04 10:37:04 +0800

    dev

--- tomcat:10.0/run.sh
+++ tomcat:10.0/run.sh
@@ -1,22 +1,4 @@
 #!/bin/bash
 
-for file in `find /data/config -name '*.sh'`
-do
-    chmod +x $file
-    sh $file
-done
-
 cd CATALINA_HOME
 bin/startup.sh &
-
-today=`date +%Y%m%d`
-while [[ true ]]
-do
-    if [ $today != `date +%Y%m%d` ];then
-        cp /data/logs/catalina.out /data/logs/catalina-$today.out
-        today=`date +%Y%m%d`
-        echo > /data/logs/catalina.out
-        find /data/logs/ -mtime +7 -exec rm -rf {} \;
-    fi
-    sleep 1s
-done
